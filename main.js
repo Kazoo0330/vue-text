e19a84c99c7ad1cfed0da947837797795df979bc
kazoo <kazuki_saito@diveintocode.jp>
2018-07-04 11:54:04 +0900

    feat: Vue.js textbook practice

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    // message: '初期メッセージ',
+    // list: ['apple', 'banana', 'strawberry'],
+    show: true
+  // },
+  // methods: {
+    // handleClick: function(event) {
+      // alert(event.target)
+    // }
+  }
+})
+console.log(app.message)
