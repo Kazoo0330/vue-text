a6349489f698eea9c7acd0bda0e334bec7bdfd80
kazoo <kazuki_saito@diveintocode.jp>
2018-07-18 12:12:03 +0900

    feat: p62~63

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 new Vue({
   el: '#app',
   data: {
-    count: 0
-  },
-  methods: {
-    // handler for clicking the button
-    increment: function(){
-      this.count += 1  //処理は再代入のみ
-    }
+    isChild: true,
+    isActive: true,
+    textColor: 'red',
+    bgColor: 'lightgray'
   }
 })
